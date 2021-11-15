9973cc08bde143e6307e0c22c9332e9ffe9e785a
ABernard <90515331+ABernard27@users.noreply.github.com>
2021-11-15 18:46:45 +0100

    Update CODE.py

--- Coberny/CODE.py
+++ Coberny/CODE.py
@@ -7,7 +7,6 @@ from download import download
 from pyproj import Proj, transform
 
 pd.options.display.max_rows = 50
-
 #%%
 url= 'https://static.data.gouv.fr/resources/gares-de-peage-du-reseau-routier-national-concede/20210224-175626/gares-peage-2019.csv'
 path_target = './gares-peage-2019.csv'
@@ -25,14 +24,14 @@ data = data.loc[(data['x']=='767254,1') | (data[" Nom gare "]=='SETE            
 (data[" Nom gare "]=='CARCASSONNE EST') | (data[" Nom gare "]=='CARCASSONNE OUEST') | (data[" Nom gare "]=='BRAM') | (data[" Nom gare "]=='CASTELNAUDARY') | (data[" Nom gare "]=='VILLEFRANCHE DE LAURAGAIS') | 
 (data[" Nom gare "]=='NAILLOUX') | (data[" Nom gare "]=='MAZERES') | (data[" Nom gare "]=='PAMIER') | (data[" Nom gare "]=='TOULOUSE SUD-OUEST (ENTREE)') | (data[" Nom gare "]=='TOULOUSE SUD-EST (SORTIE )')
 | (data[" Nom gare "]=='PERPIGNAN NORD                          ')]
-
 #%%
 data.info(verbose=True)
 data.select_dtypes(include=['float64'])
 data = data.stack().str.replace(',','.').unstack()
 data = data.set_index(np.arange(len(data)))
 
-# transformation coordonnées lambert93 en coordonnées WGS84
+# transformation des coordonnées lambert93 en coordonnées WGS84
+
 inProj = Proj(init='epsg:2154')
 outProj = Proj(init='epsg:4326')
 L93 = np.array([data['x'],data['y']]).T
@@ -81,13 +80,15 @@ print(DIST)
 # source : https://ichi.pro/fr/distance-parcourue-entre-deux-ou-plusieurs-endroits-en-python-151146835025391
 
 # %%
-url = 'https://raw.githubusercontent.com/ABernard27/PROJET-groupe-3/master/Doc/prix.csv'
+url = 'https://raw.githubusercontent.com/ABernard27/PROJET-groupe-3/Developpement/Doc/prix.csv'
 path_target = "./prix.csv"
 download(url, path_target, replace=False)
 
 prix = pd.read_csv("prix.csv", sep=';',usecols=['St-Jean-de-Vedas','Sete','Agde Pezenas','Peage de Beziers-Cabrials','Beziers ouest','Narbonne est ',
-'Narbonne sud','Sigean ','Leucate','Perpignan nord','Perpignan sud','Le Boulou  (peage sys ferme)','Peage du Perthus','Frontiere Espagnole','Lezignan',
-'Carcassonne est','Carcassonne ouest','Bram','Castelnaudary','Villefranche-de-Lauragais','Nailloux','Mazeres-Saverdun','Peage de pamiers','Montgiscard','Peage de Toulouse sud/ouest'])
+'Narbonne sud','Sigean ','Leucate','Perpignan nord','Perpignan sud','Le Boulou  (peage sys ferme)','Peage du Perthus','Lezignan',
+'Carcassonne est','Carcassonne ouest','Bram','Castelnaudary','Villefranche-de-Lauragais','Nailloux','Mazeres-Saverdun','Peage de pamiers','Montgiscard','Peage de Toulouse sud/ouest','Peage de Toulouse sud/est'])
 
 prix = prix.drop(prix.index[[0,1,2,3,5,18,29,30,33,34,35,36,37,38,39,40,41,42]])
-prix = prix.set_index(np.arange(len(prix)))
+prix.index=['St-Jean-de-Vedas','Sete','Agde Pezenas','Peage de Beziers-Cabrials','Beziers ouest','Narbonne est ',
+'Narbonne sud','Sigean ','Leucate','Perpignan nord','Perpignan sud','Le Boulou  (peage sys ferme)','Peage du Perthus','Lezignan',
+'Carcassonne est','Carcassonne ouest','Bram','Castelnaudary','Villefranche-de-Lauragais','Nailloux','Mazeres-Saverdun','Peage de pamiers','Montgiscard','Peage de Toulouse sud/ouest','Peage de Toulouse sud/est']
