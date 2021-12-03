85a20cb91353a866aff526de480a02e9bd660174
Anne Bernard <anne.bernard@etu.umontpellier.fr>
2021-12-03 18:06:25 +0100

    update

--- Coberny/Données.py
+++ Coberny/Données.py
@@ -99,9 +99,9 @@ print("Temps passé pour exécuter la commande: {0:.5f} s.".format(end2 - start2
 start3 = time.time()
 url = 'https://raw.githubusercontent.com/ABernard27/PROJET-groupe-3/Developpement/Document/tarif.csv'
 path_target = "./tarif.csv"
-download(url, path_target, replace=False)
+download(url, path_target, replace = False)
 
-prix = pd.read_csv("tarif.csv", sep=';', usecols=['St-Jean-de-Vedas', 'Sete', 'Agde Pezenas', 'Peage de Beziers-Cabrials', 'Beziers ouest', 'Narbonne est ',
+prix = pd.read_csv("tarif.csv", sep=';', usecols = ['St-Jean-de-Vedas', 'Sete', 'Agde Pezenas', 'Peage de Beziers-Cabrials', 'Beziers ouest', 'Narbonne est ',
 'Narbonne sud', 'Sigean ', 'Leucate', 'Perpignan nord', 'Perpignan sud', 'Le Boulou  (peage sys ferme)', 'Peage du Perthus', 'Lezignan',
 'Carcassonne est', 'Carcassonne ouest', 'Bram', 'Castelnaudary', 'Villefranche-de-Lauragais', 'Nailloux', 'Mazeres-Saverdun', 'Peage de pamiers', 'Montgiscard', 'Peage de Toulouse sud/ouest', 'Peage de Toulouse sud/est'])
 
