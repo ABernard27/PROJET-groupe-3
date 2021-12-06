9331555dcd01e0d146d3d47a0ba4d13609ace841
Anne Bernard <anne.bernard@etu.umontpellier.fr>
2021-12-06 15:12:05 +0100

    update

--- Coberny/distribution_of_price/distribution.py
+++ Coberny/distribution_of_price/distribution.py
@@ -21,9 +21,7 @@ class distribution(object):
         Le nom des colonnes et des lignes du dataframe Prix doivent être les noms des villes.
         Les colonnes du dataframe Distance doivent être le nom des villes, et les lignes doivent être indicées de 0 à n-1, avec n qui est égale au nombre de villes du dataframe.
 
-    :returns: Le Kernel Density Estimate (KDE): l'estimation de la fonction de densité de la variable prix/kilomètre sur la portion d'autoroute entre l'entrée et la sortie choisies.
-    Le bar plot des prix au kilomètre entre chaque portion de l'autoroute. 
-    
+    :returns: Le Kernel Density Estimate (KDE): l'estimation de la fonction de densité de la variable prix/kilomètre sur la portion d'autoroute entre l'entrée et la sortie choisies. Le bar plot des prix au kilomètre entre chaque portion de l'autoroute. 
     """
     def __init__(self,Distance,Prix):
         self.Distance = Distance
