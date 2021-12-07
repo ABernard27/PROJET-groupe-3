2ad70f56ed34f5fbfcf68b822bcc9e28ea6c22ab
ABernard <90515331+ABernard27@users.noreply.github.com>
2021-12-07 17:15:28 +0100

    Update __init__.py

--- Coberny/__init__.py
+++ Coberny/__init__.py
@@ -1,5 +1,6 @@
 __version__ = '1.0.3'
 
+from .io.indice import indice
 from .map.Carte import carte
 from .distribution_of_price.distribution import distribution
 from .graph_min_cost.best_price_path import *
