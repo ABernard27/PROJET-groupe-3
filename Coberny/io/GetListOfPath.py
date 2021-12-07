6d68dc825eca986807d1311f83a3b07c02c6f43f
ABernard <90515331+ABernard27@users.noreply.github.com>
2021-12-07 17:07:21 +0100

    Delete Coberny/io directory

--- Coberny/io/GetListOfPath.py
+++
@@ -1,9 +0,0 @@
-import Coberny as cyb
-import itertools
-
-def GetListOfPath(data, entrance, outlet, nbr_exit):
-    listOfExit = cyb.GetListOfPossibleExit(data, entrance, outlet)
-    listOfPath = list(itertools.combinations(listOfExit, nbr_exit))
-    for i in range(len(listOfPath)):
-        listOfPath[i] = (entrance,) + listOfPath[i] + (outlet,)
-    return listOfPath
--- Coberny/io/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-
-url_dist = "https://raw.githubusercontent.com/ABernard27/PROJET-groupe-3/master/Document/Distance.csv"
-url_prix = "https://raw.githubusercontent.com/ABernard27/PROJET-groupe-3/master/Document/Prix.csv"
-path_dist = os.path.join(os.getcwd(), 'Distance.csv')
-path_price = os.path.join(os.getcwd(), 'Prix.csv')
