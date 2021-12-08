fb5f4bca8e7f6882a9f5a636b250c7112299f054
ABernard <90515331+ABernard27@users.noreply.github.com>
2021-12-08 14:15:08 +0100

    Update test_Coberny.py

--- Coberny/test/test_Coberny.py
+++ Coberny/test/test_Coberny.py
@@ -7,7 +7,7 @@ import pandas as pd
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + (os.path.sep + '..')*2)
 
 import Coberny as cyb
-from Coberny.io import *
+from Coberny.url import *
 
 def test_indice1():   
     download(url_dist, path_dist, replace=True)
