61868434e59e43dc66224b5d7ff65e67f122c8d1
ABernard <90515331+ABernard27@users.noreply.github.com>
2021-12-08 14:08:55 +0100

    Create __init__.py

---
+++ Coberny/io/__init__.py
@@ -0,0 +1,6 @@
+import os 
+
+url_dist = "https://raw.githubusercontent.com/ABernard27/PROJET-groupe-3/master/Document/Distance.csv"
+url_prix = "https://raw.githubusercontent.com/ABernard27/PROJET-groupe-3/master/Document/Prix.csv"
+path_dist = os.path.join(os.getcwd(),"Distance.csv")
+path_prix = os.path.join(os.getcwd(), "Prix.csv")
