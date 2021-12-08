82895b3500a112d8e2a7f094342b69ee05a3d007
FannyChery <90441741+FannyChery@users.noreply.github.com>
2021-12-08 14:22:27 +0100

    Update test_Coberny.py

--- Coberny/test/test_Coberny.py
+++ Coberny/test/test_Coberny.py
@@ -12,16 +12,16 @@ from Coberny.url import *
 def test_indice1():   
     download(url_dist, path_dist, replace=True)
     DISTANCE=pd.read_csv("Distance.csv", sep=',')
-    assert cyb.indice('SETE', DISTANCE) == 1
+    assert cyb.indice('SETE', DISTANCE) == 25
 
 
 def test_indice2():
     download(url_dist, path_dist, replace=True)
     DISTANCE=pd.read_csv("Distance.csv", sep=',')
-    assert cyb.indice('CASTELNAUDARY', DISTANCE) == 17
+    assert cyb.indice('CASTELNAUDARY', DISTANCE) == 16
 
 
 def test_indice3():
     download(url_dist, path_dist, replace=True)
     DISTANCE=pd.read_csv("Distance.csv", sep=',')
-    assert cyb.indice('MONTGISCARD', DISTANCE) == 22 
+    assert cyb.indice('MONTGISCARD', DISTANCE) == 21 
