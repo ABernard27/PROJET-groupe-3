2088760d06e3052f855a1104b003c0e09d41c21e
ABernard <90515331+ABernard27@users.noreply.github.com>
2021-12-08 14:10:54 +0100

    Update test_Coberny.py

--- Coberny/test/test_Coberny.py
+++ Coberny/test/test_Coberny.py
@@ -7,20 +7,21 @@ import pandas as pd
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + (os.path.sep + '..')*2)
 
 import Coberny as cyb
+from Coberny.io import *
 
 def test_indice1():   
-    download(cyb.url_dist, cyb.path_dist, replace=True)
+    download(url_dist, path_dist, replace=True)
     DISTANCE=pd.read_csv("Distance.csv", sep=',')
     assert cyb.indice('Sete', DISTANCE) == 1
 
 
 def test_indice2():
-    download(cyb.url_dist, cyb.path_dist, replace=True)
+    download(url_dist, path_dist, replace=True)
     DISTANCE=pd.read_csv("Distance.csv", sep=',')
     assert cyb.indice('Castelnaudary', DISTANCE) == 17
 
 
 def test_indice3():
-    download(cyb.url_dist, cyb.path_dist, replace=True)
+    download(url_dist, path_dist, replace=True)
     DISTANCE=pd.read_csv("Distance.csv", sep=',')
     assert cyb.indice('Montgiscard', DISTANCE) == 22 
