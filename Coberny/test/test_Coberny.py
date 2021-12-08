a19a8e5db70a6dbdeee88147f995a15cdb18b05a
FannyChery <90441741+FannyChery@users.noreply.github.com>
2021-12-08 14:37:41 +0100

    Update test_Coberny.py

--- Coberny/test/test_Coberny.py
+++ Coberny/test/test_Coberny.py
@@ -12,7 +12,7 @@ from Coberny.url import *
 def test_indice1():   
     download(url_dist, path_dist, replace=True)
     DISTANCE=pd.read_csv("Distance.csv", sep=',')
-    assert cyb.indice('AGDE', DISTANCE) == 24
+    assert cyb.indice('AGDE', DISTANCE) == 2
 
 
 def test_indice2():
