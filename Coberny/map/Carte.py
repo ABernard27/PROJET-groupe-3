feba1d0193923aae64618229b1ec927b9a8da300
Anne Bernard <anne.bernard@etu.umontpellier.fr>
2021-12-09 11:05:55 +0100

    update

--- Coberny/map/Carte.py
+++ Coberny/map/Carte.py
@@ -26,8 +26,12 @@ def carte(Coord, Name, Key, prix):
     	Coberny.carte(np.column_stack([data['x'],data['y']]), data[' Nom gare' ], '5b3ce3597851110001cf6248dfd20bf8793e4e978b4bb0ca383bfd55', prix)
     """
     start = time.time()
-    client = openrouteservice.Client(key=Key)
+    client = openrouteservice.Client(key=Key) 
+    #utilise la clé openrouteservice pour pouvoir utiliser les données cartographiques
     m = folium.Map(location=[43.1837661, 3.0042121], zoom_start=10, control_scale=True)
+    #se place autour du point donné qui est Narbonne ici
+    
+    # la boucle va faire des requêtes de chaque point de coordonnée entré puis relier ces points en passant par l'autoroute
     for i in range(0, len(Coord)-1):
         coords = (tuple(Coord[i, :]), )+(tuple(Coord[i+1, :]), )
         res = client.directions(coords)
@@ -36,16 +40,17 @@ def carte(Coord, Name, Key, prix):
             f.write(json.dumps(res, indent=4, sort_keys=True))
 
         geometry = client.directions(coords)['routes'][0]['geometry']
+        # decode_polyline est une fonction pour relier les points
         decoded = convert.decode_polyline(geometry)
         distance_txt = "<h4> <b>Distance :&nbsp" + "<strong>" + str(round(res['routes'][0]['summary']['distance'] / 1000, 1)) + " Km </strong>" + "</h4></b>"
         duration_txt = "<h4> <b>Duration :&nbsp" + "<strong>"+ str(round(res['routes'][0]['summary']['duration'] / 60, 1)) + " Mins. </strong>" + "</h4></b>"
         test = "<h4> <b>Prix :&nbsp" + "<strong>"+ str(prix[Name[i]][i+1]) + "€ </strong>" + "</h4></b>"
-        folium.GeoJson(decoded).add_child(folium.Popup(test+distance_txt+duration_txt, max_width=500)).add_to(m)
+        folium.GeoJson(decoded).add_child(folium.Popup(test+distance_txt+duration_txt, max_width=500)).add_to(m) # ajout des données à une carte existante neutre
         folium.Marker(
         location=list(coords[0][::-1]),
         popup=Name[i],
         icon=folium.Icon(color='orange', icon='car', prefix='fa'),
-        ).add_to(m)
+        ).add_to(m) # folium.Marker pour noter et nommer les points de coordonnées
     m.save('map.html')
     end = time.time()
     print("Temps passé pour exécuter la commande: {0:.5f} s.".format(end - start))
