276a9a595bea0d56e525f7f31b0d3ac2a624a139
FannyChery <90441741+FannyChery@users.noreply.github.com>
2021-12-10 16:12:14 +0100

    Update distribution.py

--- Coberny/distribution_of_price/distribution.py
+++ Coberny/distribution_of_price/distribution.py
@@ -28,10 +28,10 @@ class distribution(object):
         self.Prix = Prix
     
     def indice(l, DISTANCE):
-    for i in range(DISTANCE.shape[0]):
-        if (DISTANCE.columns[i+1] == l):
-            return i 
-        else: i = i + 1
+        for i in range(DISTANCE.shape[0]):
+            if (DISTANCE.columns[i+1] == l):
+                return i 
+            else: i = i + 1
 
     
     # Plot le KDE: la distribution
