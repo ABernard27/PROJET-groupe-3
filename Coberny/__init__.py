bfa70d2dee04b5a955118f1581f8e21f924803cf
Olivier97400 <91317392+Olivier97400@users.noreply.github.com>
2021-12-13 23:30:03 +0100

    Update __init__.py

--- Coberny/__init__.py
+++ Coberny/__init__.py
@@ -1,6 +1,8 @@
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 
 from .map.Carte import carte
 from .distribution_of_price.distribution import *
 from .graph_min_cost.best_price_path import *
+from .graph_min_cost.bestPricePathForUI import GraphOfBestPathForPrice_UI
+from .graph_min_cost.best_price_pathUI import *
 from .distribution_of_price.ind import ind
