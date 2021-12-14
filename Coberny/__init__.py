9a599ea527a0e24e7a66c5e594b7305f8c524013
FannyChery <90441741+FannyChery@users.noreply.github.com>
2021-12-14 19:32:18 +0100

    Update __init__.py

--- Coberny/__init__.py
+++ Coberny/__init__.py
@@ -4,6 +4,6 @@ from .map.Carte import carte
 from .distribution_of_price.distribution import *
 from .graph_min_cost.best_price_path import *
 from .graph_min_cost.bestPricePathForUI import *
-from .graph_min_cost.best_price_pathUI import *
+from .graph_min_cost.best_price_path import *
 from .graph_min_cost.AppFindBestPathForPrice import *
 from .distribution_of_price.ind import ind
