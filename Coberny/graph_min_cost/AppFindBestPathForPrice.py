29e353717066f0ca783127156748636fc90f956c
FannyChery <90441741+FannyChery@users.noreply.github.com>
2021-12-14 20:35:03 +0100

    Update AppFindBestPathForPrice.py

--- Coberny/graph_min_cost/AppFindBestPathForPrice.py
+++ Coberny/graph_min_cost/AppFindBestPathForPrice.py
@@ -158,99 +158,99 @@ def UI():
 
 
 
-            def loadDataFile(self):
-                self.nameFile.set(askopenfilename(title="Choisir le fichier de données",
-                                                  filetypes=[('csv files', '.csv')]))
-                self.data = pd.read_csv(self.nameFile.get())
-                self.data = self.data.fillna(0)
-                if (not self.data.empty):
-                    self.button_loadDepartCity.configure(state='normal')
+        def loadDataFile(self):
+            self.nameFile.set(askopenfilename(title="Choisir le fichier de données",
+                                              filetypes=[('csv files', '.csv')]))
+            self.data = pd.read_csv(self.nameFile.get())
+            self.data = self.data.fillna(0)
+            if (not self.data.empty):
+                self.button_loadDepartCity.configure(state='normal')
 
 
-            def loadDepartCity(self):
-                departCityList = cyb.GetListOfcolnames(self.data)
-                self.cbox_departCity['values'] = tuple(departCityList)
-                self.cbox_departCity.current(0)
-                self.cbox_departCity.configure(state='readonly')
+        def loadDepartCity(self):
+            departCityList = cyb.GetListOfcolnames(self.data)
+            self.cbox_departCity['values'] = tuple(departCityList)
+            self.cbox_departCity.current(0)
+            self.cbox_departCity.configure(state='readonly')
 
 
-            def loadDestinationCity(self):
-                departCityList = cyb.GetListOfcolnames(self.data)
-                destinaCityList = departCityList[:]
-                destinaCityList.remove(self.cbox_departCity.get())
-                self.cbox_destinaCity['values'] = tuple(destinaCityList)
-                self.cbox_destinaCity.current(0)
-                self.cbox_destinaCity.configure(state='readonly')
-                self.button_stepsOnPath.configure(state='normal')
+        def loadDestinationCity(self):
+            departCityList = cyb.GetListOfcolnames(self.data)
+            destinaCityList = departCityList[:]
+            destinaCityList.remove(self.cbox_departCity.get())
+            self.cbox_destinaCity['values'] = tuple(destinaCityList)
+            self.cbox_destinaCity.current(0)
+            self.cbox_destinaCity.configure(state='readonly')
+            self.button_stepsOnPath.configure(state='normal')
 
 
-            def findStepsOnPath(self):
-                kMaxConstraint = cyb.GetKMaxConstraint(self.data,
-                                                           self.departureCity.get(),
-                                                           self.destinationCity.get())
-                stepsNb_list = [x for x in range(0, kMaxConstraint + 1)]
-                self.cbox_stepsNumber['values'] = tuple(stepsNb_list)
-                self.cbox_stepsNumber.current(0)
-                self.cbox_stepsNumber.configure(state='readonly')
-                self.button_bestPathForPrice.configure(state='normal')
+        def findStepsOnPath(self):
+            kMaxConstraint = cyb.GetKMaxConstraint(self.data,
+                                                       self.departureCity.get(),
+                                                       self.destinationCity.get())
+            stepsNb_list = [x for x in range(0, kMaxConstraint + 1)]
+            self.cbox_stepsNumber['values'] = tuple(stepsNb_list)
+            self.cbox_stepsNumber.current(0)
+            self.cbox_stepsNumber.configure(state='readonly')
+            self.button_bestPathForPrice.configure(state='normal')
 
 
-            def bestPathForPrice(self):
-                self.winResult.insert(tk.INSERT, '*************************************\n')
-                self.winResult.insert(tk.END, '*************************************\n')
-                startTime1 = time.time()
-                bestPathPriceCouple = cyb.FindBestPathForPrice(self.data,
-                                                                   self.departureCity.get(),
-                                                                   self.destinationCity.get(),
-                                                                   self.stepsNumber.get())
-                runTime1 = time.time() - startTime1
-                roundRunTime1 = str(dt.timedelta(seconds=runTime1))
-                beginStr1 = "Temps d'exécution: "+ str(runTime1) +' secondes.\n cad '
-                endStr1 = str(roundRunTime1) + " dans le format heures minutes secondes"
-                displayTime1 = beginStr1 + endStr1
+        def bestPathForPrice(self):
+            self.winResult.insert(tk.INSERT, '*************************************\n')
+            self.winResult.insert(tk.END, '*************************************\n')
+            startTime1 = time.time()
+            bestPathPriceCouple = cyb.FindBestPathForPrice(self.data,
+                                                               self.departureCity.get(),
+                                                               self.destinationCity.get(),
+                                                               self.stepsNumber.get())
+            runTime1 = time.time() - startTime1
+            roundRunTime1 = str(dt.timedelta(seconds=runTime1))
+            beginStr1 = "Temps d'exécution: "+ str(runTime1) +' secondes.\n cad '
+            endStr1 = str(roundRunTime1) + " dans le format heures minutes secondes"
+            displayTime1 = beginStr1 + endStr1
 
-                bestPath = bestPathPriceCouple[0]
-                bestPrice = bestPathPriceCouple[1]
-                self.winResult.insert(tk.END, 'Meilleur trajet: ' + str(bestPath) + '\n')
-                self.winResult.insert(tk.END, 'Meilleur prix: ' + str(bestPrice) + '\n')
-                self.winResult.insert(tk.END, displayTime1 + '\n')
-                self.winResult.insert(tk.END, '-------------------------------------\n')
-                self.winResult.insert(tk.END, 'Génération du graph en cours \n')
+            bestPath = bestPathPriceCouple[0]
+            bestPrice = bestPathPriceCouple[1]
+            self.winResult.insert(tk.END, 'Meilleur trajet: ' + str(bestPath) + '\n')
+            self.winResult.insert(tk.END, 'Meilleur prix: ' + str(bestPrice) + '\n')
+            self.winResult.insert(tk.END, displayTime1 + '\n')
+            self.winResult.insert(tk.END, '-------------------------------------\n')
+            self.winResult.insert(tk.END, 'Génération du graph en cours \n')
 
-                f = Figure(figsize=(5,5), dpi=100, facecolor='white')
-                ax = f.add_subplot(111)
-                plt.axis('off')
+            f = Figure(figsize=(5,5), dpi=100, facecolor='white')
+            ax = f.add_subplot(111)
+            plt.axis('off')
 
-                startTime2 = time.time()
-                cyb.GraphOfBestPathForPrice_UI(self.data,
-                                                      self.departureCity.get(),
-                                                      self.destinationCity.get(),
-                                                      self.stepsNumber.get(),
-                                                      ax)
-                runTime2 = time.time() - startTime2
-                roundRunTime2 = str(dt.timedelta(seconds=runTime2))
-                beginStr2 = "Temps d'exécution: "+ str(runTime2) +' secondes.\n cad '
-                endStr2 = str(roundRunTime2) + " dans le format heures minutes secondes"
-                displayTime2 = beginStr2 + endStr2
-                self.winResult.insert(tk.END, displayTime2 + '\n')
-                self.winResult.insert(tk.END, '*************************************\n')
-                self.winResult.insert(tk.END, '*************************************\n')
-                self.winResult.insert(tk.END, '\n')
-                self.winResult.insert(tk.END, '\n')
+            startTime2 = time.time()
+            cyb.GraphOfBestPathForPrice_UI(self.data,
+                                                  self.departureCity.get(),
+                                                  self.destinationCity.get(),
+                                                  self.stepsNumber.get(),
+                                                  ax)
+            runTime2 = time.time() - startTime2
+            roundRunTime2 = str(dt.timedelta(seconds=runTime2))
+            beginStr2 = "Temps d'exécution: "+ str(runTime2) +' secondes.\n cad '
+            endStr2 = str(roundRunTime2) + " dans le format heures minutes secondes"
+            displayTime2 = beginStr2 + endStr2
+            self.winResult.insert(tk.END, displayTime2 + '\n')
+            self.winResult.insert(tk.END, '*************************************\n')
+            self.winResult.insert(tk.END, '*************************************\n')
+            self.winResult.insert(tk.END, '\n')
+            self.winResult.insert(tk.END, '\n')
 
-                winGraph = tk.Toplevel(self.win)
+            winGraph = tk.Toplevel(self.win)
 
-                def destroyWindow():
-                    winGraph.destroy()
+            def destroyWindow():
+                winGraph.destroy()
 
-                winGraph.title('Le meilleur trajet au meilleur prix')
-                winGraph.minsize(width=100, height=100)
-                winGraph.protocol('WM_DELETE_WINDOW', destroyWindow)
-                canvas = FigureCanvasTkAgg(f, master=winGraph)
-                canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-                toolbar = NavigationToolbar2Tk(canvas, winGraph)
-                toolbar.update()
-                canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+            winGraph.title('Le meilleur trajet au meilleur prix')
+            winGraph.minsize(width=100, height=100)
+            winGraph.protocol('WM_DELETE_WINDOW', destroyWindow)
+            canvas = FigureCanvasTkAgg(f, master=winGraph)
+            canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+            toolbar = NavigationToolbar2Tk(canvas, winGraph)
+            toolbar.update()
+            canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
         
     app = AppFindBestPathForPrice()
