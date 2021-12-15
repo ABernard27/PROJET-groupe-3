6c1f8969cbe92c7599377c4050bd2817ef7e9997
ABernard <90515331+ABernard27@users.noreply.github.com>
2021-12-15 07:57:35 +0100

    Update AppFindBestPathForPrice.py

--- Coberny/graph_min_cost/AppFindBestPathForPrice.py
+++ Coberny/graph_min_cost/AppFindBestPathForPrice.py
@@ -18,9 +18,7 @@ import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 
-def UI():
-    
-    
+
     class AppFindBestPathForPrice():
         # Ici on initialise la fenêtre de l'interface
         def __init__(self):
